253569ae3cf6132ae3b001f193e41929d7290415
ayan471 <pakhiraayan@gmail.com>
2023-07-26 19:18:20 +0530

    creating orders entity

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,6 +17,11 @@ export function MainNav({
       active: pathname === `/${params.storeId}`,
     },
     {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
       href: `/${params.storeId}/categories`,
       label: "Categories",
       active: pathname === `/${params.storeId}/categories`,
@@ -32,9 +37,9 @@ export function MainNav({
       active: pathname === `/${params.storeId}/colors`,
     },
     {
-      href: `/${params.storeId}/billboards`,
-      label: "Billboards",
-      active: pathname === `/${params.storeId}/billboards`,
+      href: `/${params.storeId}/products`,
+      label: "Products",
+      active: pathname === `/${params.storeId}/products`,
     },
     {
       href: `/${params.storeId}/settings`,
